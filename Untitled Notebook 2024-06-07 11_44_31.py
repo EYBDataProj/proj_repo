baacffa809cf11195bd3772b13ee3c5d24132a84
Eybdataproj <Eybdataproj>
2024-06-07 10:03:47 +0000

    test notebook

---
+++ Untitled Notebook 2024-06-07 11_44_31.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+import pandas as pd
+
+# COMMAND ----------
+
+test_df = pd.read_csv('data/all_fuels_data.csv')
+test_df.head()
+
+# COMMAND ----------
+
+test_df.plot.bar(y='volume')
+
+# COMMAND ----------
+
+
